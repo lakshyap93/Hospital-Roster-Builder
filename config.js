ef333a835379af2fb50fb8090a42018e40755969
Lakshya Purohit <lakshyap93@gmail.com>
2026-08-19 01:06:32 +0530

    delete

--- config.js
+++
@@ -1,9 +0,0 @@
-// Runtime Environment Configuration
-// IMPORTANT: Keep this file listed in .gitignore so keys are never checked into source control.
-// Only use the Anon / Publishable key in frontend applications.
-// NEVER put your SUPABASE_SERVICE_ROLE_KEY or confidential secret keys in this file!
-
-window.__ENV__ = {
-  SUPABASE_URL: "https://bumjwrmwzwzjqdejnqca.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_vgUSK9MQncLu_qLd_LEPMA_XKW9fVRJ"
-};
